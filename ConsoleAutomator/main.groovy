934def49768f4735a5a3eef2daa9da23a93904c7
juanheredia <juanheredia@borsan.es>
2021-10-10 20:09:34 +0200

    Add Groovy

---
+++ ConsoleAutomator/main.groovy
@@ -0,0 +1,22 @@
+﻿import hudson.model.*
+
+def consoleautomator(Map parameters) {
+    stage ('Git Checkout') {
+        node {
+          git branch: 'master', credentialsId: 'GitLabHTTP', 
+          url: 'https://gitlab.com/juanheredia/consoleautomator.git'
+        }
+    }
+    stage ('Build .NET') {
+        node {
+            sh 'dotnet build'
+        }
+    }
+    stage ('Run .NET') {
+        node {
+            sh 'ConsoleAutomator/bin/Debug/net5.0/ConsoleAutomator'
+        }
+    }
+}
+
+return this
