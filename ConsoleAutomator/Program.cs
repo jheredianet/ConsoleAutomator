e154b43c4ef607ff92d4d4f89b19b56547d256fe
juanheredia <juanheredia@borsan.es>
2021-10-11 18:18:26 +0200

    Modify Exec for console

--- ConsoleAutomator/Program.cs
+++ ConsoleAutomator/Program.cs
@@ -8,34 +8,36 @@ namespace ConsoleAutomator
     {
         static void Main(string[] args)
         {
+            if (Environment.GetCommandLineArgs().Length != 3)
+            {
+                Console.WriteLine("Use this program with 2 parameters: FileName and Arguments");
+                return;
+            }
+
+            var FileName = Environment.GetCommandLineArgs()[1];
+            var Arguments = Environment.GetCommandLineArgs()[2];
+
+
             Console.WriteLine("Data from Server Environmet:");
             Console.WriteLine();
             Console.WriteLine("GetEnvironmentVariables: ");
             foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
                 Console.WriteLine("  {0} = {1}", de.Key, de.Value);
 
-            var command = "/var/jenkins_home/";
-            var proc = new Process
+            var process = new Process()
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "find",/// @"C:\Windows\System32\cmd.exe",
-                    Arguments = command,
-                    RedirectStandardInput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
+                    FileName = FileName,
+                    Arguments = Arguments
+                    //RedirectStandardOutput = true,
+                    //UseShellExecute = false,
+                    //CreateNoWindow = true,
                 }
             };
 
-            proc.EnableRaisingEvents = true;
-            proc.OutputDataReceived += (s, e) => Debug.WriteLine(e.Data);
-            proc.ErrorDataReceived += (s, e) => Debug.WriteLine($@"Error: {e.Data}");
-            proc.Start();
-            proc.BeginOutputReadLine();
-            proc.BeginErrorReadLine();
-            proc.WaitForExit();
+            process.Start();
+            process.WaitForExit();
 
 
 
